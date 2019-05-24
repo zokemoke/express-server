cac819092ec5990a38b8114733ef59656cefbc62
Oak Isarapong <ozaaaaa@gmail.com>
2019-05-24 15:32:28 +0700

    add project socket UDCH

--- server.js
+++ server.js
@@ -1,19 +1,41 @@
-import express from 'express'
-import bodyParser from 'body-parser'
-const server = express()
-const port = 9000;
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+var env = require('node-env-file');
+const fetch = require('node-fetch');
+var port = 3000;
+env(__dirname + '/.env');
 
-server.use(bodyParser.json())
-server.use(bodyParser.urlencoded({
-    extended: true
-}))
+io.on('connection', socket => {
+    console.log('New user connected -- Socket UDCH server');
+    const sendEmit = (event, data) => {
+        console.log('broadcast emit event:' + event);
+        socket.broadcast.emit(event, data);
+    };
 
-server.get('/', (req, res) => {
-    res.send('ok')
-})
+    const actionFetch = (event, url) => {
+        fetch(url).then(response => response.json()).then(data => {
+            console.log(data);
+            if (data.data.order_complte) {
+                sendEmit(event, data);
+            }
+        });
+    };
 
-server.listen(port, function (err, result) {
+    socket.on('checkOrderChemo', (data) => {
+        const params = new URLSearchParams({
+            visit_id: data.visit_id
+        });
+        let mainUrl = process.env.backendUrl + 'api/public-thai-his/order-chemo-complte?' + params
+        actionFetch('newOrderChemo', mainUrl);
+    });
+
+    socket.on('disconnect', function () {
+        console.log('disconnected server');
+        io.emit('disconnected', { 'message': 'Bey' });
+    });
+});
+
+http.listen(port, function () {
     console.log('running in port http://localhost:' + port)
-})
-
-export default server
+});
